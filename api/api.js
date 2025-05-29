f7f0422afb5e13e2bf55acaf21d68981ee86ea53
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2025-05-29 11:11:18 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,9 +1,12 @@
-const quotes = require('./quotes.json');
+const path = require('path'); // 引入 path 模块
+
+// 使用 process.cwd() 来获取当前工作目录，并构建 quotes.json 的绝对路径
+const quotes = require(path.join(process.cwd(), 'quotes.json'));
 
 module.exports = async (req, res) => {
   // 随机选择一条名句
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  
+
   res.status(200).json({
     quote: randomQuote,
     source: "佛经",
